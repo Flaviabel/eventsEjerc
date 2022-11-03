2a69e8814dd385ebf1c95a66b96846be619b8d37
Flaviabel <flaviamanfredi521@gmail.com>
2022-11-03 17:06:31 +0100

    first commit

---
+++ ejer/main.js
@@ -0,0 +1,20 @@
+// function countRabbit(){
+//     for (let i = 0; i < array.length; i++) {
+//         alert(`Conejor numero ${i}`)
+         
+
+//     } 
+// }
+
+
+function removeElement(){
+    return text.remove()
+}
+hider.addEventListener("click",removeElement)
+
+
+function removeClick(){
+    return borrar.remove()
+}
+document.addEventListener("click",removeClick)
+
---
+++ ejerciciobotondecierre/main.js
@@ -0,0 +1,41 @@
+// let lorem1 = document.getElementById("lorem1")
+
+// function removeElement(){
+//     return lorem1.remove()
+// }
+// removeClick1.addEventListener("click",removeElement)
+
+
+// let lorem2= document.getElementById("lorem2")
+// function removeElement(){
+//     return lorem2.remove()
+// }
+// removeClick2.addEventListener("click",removeElement)
+
+
+
+// let lorem3= document.getElementById("lorem3")
+// function removeElement(){
+//     return lorem3.remove()
+// }
+// removeClick3.addEventListener("click",removeElement)
+
+
+// function removeElement() {
+//     for (let i = 0; i <= lorem; i++) {
+//        console.log(lorem)
+//     }
+// }
+// removeClick.addEvenListener("click", removeElement)
+
+removeClick1.onclick= function(){
+    lorem1.hidden= true
+}
+removeClick2.onclick= function(){
+    lorem2.hidden= true
+}
+removeClick3.onclick= function(){
+    lorem3.hidden= true
+}
+
+// hidden = true : El elemento está oculto.
